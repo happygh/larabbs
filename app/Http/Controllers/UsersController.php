a306a9e1a9088d8ddbd07651dcc03ffa956d546a
dongcaigh <dongcai5567@gmail.com>
2018-05-08 10:36:44 +0800

    Basic user profile edit

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,13 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest;
 
 class UsersController extends Controller
 {
-    //
+    //显示个人信息
     public function show(User $user)
     {
         return view('users.show', compact('user'));
+    }
+
+    //编辑个人信息页面
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    //处理个人信息修改 自定义UserRequest
+    public function update(UserRequest $request, User $user)
+    {
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success', '个人信息修改成功');
     }
 }
