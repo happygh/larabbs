b717c2c690725346d67adc5fb883020a371f4ceb
dongcaigh <dongcai5567@gmail.com>
2018-05-08 12:01:35 +0800

    User can upload avatar

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Handlers\ImageUploadHandler;
 use App\Http\Requests\UserRequest;
+
 
 class UsersController extends Controller
 {
@@ -20,9 +22,19 @@ class UsersController extends Controller
     }
 
     //处理个人信息修改 自定义UserRequest
-    public function update(UserRequest $request, User $user)
+    public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
-        $user->update($request->all());
+        $data = $request->all();
+        if ($request->avatar)
+        {
+            $rst = $uploader->save($request->avatar, 'avatar', $user->id, 362);
+            if ($rst)
+            {
+                $data['avatar'] = $rst['path'];
+            }
+        }
+        //dd($request->file('avatar'));
+        $user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人信息修改成功');
     }
 }
