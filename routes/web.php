c80f29a141bbd529c6dc10e1ccbb6c410eb660ca
dongcaigh <dongcai5567@gmail.com>
2018-05-11 11:58:22 +0800

    Auto translate slug

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,10 @@ Auth::routes();
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
 //话题资源路由
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+//给show方法添加Slug
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
+
 
 //分类资源路由
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
