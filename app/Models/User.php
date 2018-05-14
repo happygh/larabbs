cac3728c9f5f0e01eba60fc839d3e1eca437284d
dongcaigh <dongcai5567@gmail.com>
2018-05-14 09:58:26 +0800

    Notify author

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Auth;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable{
+        notify as protected laravelNotify;
+    }
 
     /**
      * The attributes that are mass assignable.
@@ -26,6 +29,24 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function notify($instance)
+    {
+        // 如果要通知的人是当前用户，就不必通知了！
+        if ($this->id == Auth::id()) {
+            return;
+        }
+        $this->increment('notification_count');
+        $this->laravelNotify($instance);
+    }
+
+    //清除未读
+    public function markAsRead()
+    {
+        $this->notification_count = 0;
+        $this->save();
+        $this->unreadNotifications->markAsRead();
+    }
 
     public function topics()
     {
--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,5 @@ Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload
 
 //回复资源路由
 Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
+//通知路由
+Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
